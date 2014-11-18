718676bf25e2e01fc80bae034a38327da347150d
Levhita <levhita@gmail.com>
2014-11-18 06:10:21 +0000

    Lighter data structure reduced each document from 4k to 360bytes

--- TwitterCollector/Collector.py
+++ TwitterCollector/Collector.py
@@ -11,12 +11,27 @@ class StdOutListener(tweepy.StreamListener):
         global db
         
         decoded = json.loads(data)
-        db.twitts.insert(decoded)
-        
-        # Ignorar caracteres no ASCII
-        print '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
-        # twitter_mapper.twitts
-        print ''
+        if decoded['geo'] is not None:
+            processed = {}
+            processed['id_str'] = decoded['id_str']
+            processed['text'] = decoded['text']
+            processed['timestamp_ms'] = decoded['timestamp_ms']
+            processed['latitude'] = decoded['geo']['coordinates'][0]
+            processed['longitude'] = decoded['geo']['coordinates'][1]
+            processed['user_id'] = decoded['user']['id']
+            processed['user_name'] = decoded['user']['name'] 
+            processed['screen_name'] = decoded['user']['screen_name']
+            processed['picture'] = decoded['user']['profile_image_url_https']
+
+            db.twitts.insert(processed)
+            
+            # Ignorar caracteres no ASCII
+            print '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
+        else:
+
+            print '(Not Geo) @%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
+            if decoded['coordinates'] is not None:
+                print 'But coordinates exists'
         return True
 
     def on_error(self, status):
@@ -30,8 +45,8 @@ if __name__ == '__main__':
     #Tlajomulco, Tesistan, El Salto
     capture_region = [float(config.get('region', 'left')),float(config.get('region', 'bottom')), float(config.get('region', 'right')), float(config.get('region', 'top'))]
    
-    client = MongoClient(config.get('mongo', 'host'), int(config.get('mongo', 'port')))
-    db = client.twitter_mapper
+    client = MongoClient(config.get('mongo', 'connection_string'))
+    db = client[config.get('mongo', 'db_name')]
     
     l = StdOutListener()
     auth = tweepy.OAuthHandler(config.get('twitter', 'consumer_key'), config.get('twitter', 'consumer_secret'))
--- public/javascripts/mapper.js
+++ public/javascripts/mapper.js
@@ -1,5 +1,5 @@
 var map;
-var markers;
+var markers=[];
 
 function initialize() {
 	
@@ -12,22 +12,54 @@ function initialize() {
 
 }
 
+
+// Add a marker to the map and push to the array.
+function addMarker(location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+  markers.push(marker);
+}
+
+// Sets the map on all markers in the array.
+function setAllMap(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setAllMap(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setAllMap(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
+
 $(document).ready(function(){
 	initialize();
 	var iconBase = 'http://maps.google.com/mapfiles/kml/shapes';
 	$.getJSON( "twitts", function( twitts ) {
 		$.each( twitts, function(key, twitt) {
 			if(twitt.coordinates!==null) {
-				var myLatlng = new google.maps.LatLng(twitt.coordinates.coordinates[1], twitt.coordinates.coordinates[0])
+				var myLatlng = new google.maps.LatLng(twitt.latitude, twitt.longitude)
 				marker = new google.maps.Marker({
 					position: myLatlng,
 					map: map,
-					title: "@"+twitt.user.screen_name+":"+twitt.text,
+					title: "@"+twitt.screen_name+":"+twitt.text,
 					icon: 'images/FF4D00-0.8.png'
 				});
+				markers.push(marker);
 			}
 		});
-
-		
 	});
 });
