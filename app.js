40d16aaaf2caf212bbeeeaee08ad51b18229b286
Levhita <levhita@gmail.com>
2014-11-19 06:16:47 +0000

    small bug

--- app.js
+++ app.js
@@ -23,12 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/movements', movements);
+app.use('/', index);
 app.use('/tweets', tweets);
 app.use('/graphs', graphs);
-app.use('/', index);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
